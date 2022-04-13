b3aae6f6e867c260e77e4e052dec540a4c8f6dda
Valeri Sabev <valchygaming@gmail.com>
2022-04-13 20:34:16 +0200

    MVP

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import moon from './imgs/moon.png';
 import land from './imgs/land.png';
 import cat from './imgs/cat.gif';
@@ -5,14 +6,41 @@ import cat from './imgs/cat.gif';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function App() {
+	const ref = useRef();
+
 	return (
 		<div>
-			<Parallax pages={4}>
-				<ParallaxLayer>
-					<h1>Hello</h1>
+			<Parallax pages={4} ref={ref}>
+				<ParallaxLayer
+					offset={0}
+					speed={1}
+					factor={2}
+					style={{
+						backgroundImage: `url(${moon})`,
+						backgroundSize: 'cover'
+					}}
+				/>
+
+				<ParallaxLayer
+					offset={2}
+					speed={1}
+					factor={4}
+					style={{
+						backgroundImage: `url(${land})`,
+						backgroundSize: 'cover'
+					}}
+				></ParallaxLayer>
+
+				<ParallaxLayer sticky={{ start: 0.9, end: 2.5 }} style={{ textAlign: 'center' }}>
+					<img src={cat} />
 				</ParallaxLayer>
-				<ParallaxLayer>
-					<h2>World</h2>
+
+				<ParallaxLayer offset={0.2} speed={0.05} onClick={() => ref.current.scrollTo(3)}>
+					<h2>Welcome to my website</h2>
+				</ParallaxLayer>
+
+				<ParallaxLayer offset={3} speed={2} onClick={() => ref.current.scrollTo(0)}>
+					<h2>Hi Mom!</h2>
 				</ParallaxLayer>
 			</Parallax>
 		</div>
